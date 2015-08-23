7d94454f27a261d5ec249ff2819c5e53adf7a715
Kim Reece <feonixrift@gmail.com>
2015-08-22 23:21:05 -0700

    Yes, George, I broke the build.

--- test.c
+++ test.c
@@ -4,24 +4,41 @@
 #include <malloc.h>
 #include "zordinate.h"
 
-
 int main () {
   unsigned char *input = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01"\
-                         "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01"\
-                         "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01"\
                          "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01";
   unsigned char *output;
   int i,j,len,wid;
   len = strlen(input);
-  output = malloc(len * sizeof(unsigned char));
-  wid = sqrt(len);
-  if (lin2zord(input, output, len, wid)) return -1;
-  for(i=0;i<wid;i++)
+  output = calloc(len, sizeof(unsigned char));
+  //wid = sqrt(len);
+  wid = 8;
+  if (lin2zord(input, output, len, wid))
+  {
+    printf("bailing\n");
+    return -1;
+  }
+  for(i=0;i<len/wid;i++)
   {
     printf("| ");
     for(j=0;j<wid;j++)
     {
-      printf("%2x ",(int)*(output+i*wid+j));
+      printf("%c ",(char)*(output+i*wid+j));
+    }
+    printf("|\n");
+  }
+  wid = 16;
+  if (lin2zord(input, output, len, wid))
+  {
+    printf("bailing\n");
+    return -1;
+  }
+  for(i=0;i<len/wid;i++)
+  {
+    printf("| ");
+    for(j=0;j<wid;j++)
+    {
+      printf("%c ",(char)*(output+i*wid+j));
     }
     printf("|\n");
   }
--- zordinate.c
+++ zordinate.c
@@ -1,5 +1,11 @@
 #include "zordinate.h"
+#include <stdio.h>
 #include <math.h>
+
+int isPowerOfTwo (unsigned int x)
+{
+    return ((x != 0) && !(x & (x - 1)));
+}
 
 int isPowerOfFour(unsigned int n)
 {
@@ -17,33 +23,43 @@ int isPowerOfFour(unsigned int n)
 void lin2zord_internal(unsigned char *lin, int start, int end, unsigned char *zord, int zx, int zy, int zw)
 {
   int ls = sqrt(end-start+1);
-  int l4 = (end-start+1)/4;
-  if (l4 == 1)
-  {
-    *(zord+(zx+0)+(zy+0)*zw) = lin[start+0];
-    *(zord+(zx+1)+(zy+0)*zw) = lin[start+1];
-    *(zord+(zx+0)+(zy+1)*zw) = lin[start+2];
-    *(zord+(zx+1)+(zy+1)*zw) = lin[start+3];
-  } else {
-    if (ls > zw) { // The result is not square & we're in a top layer
-      lin2zord_internal(lin, start,      start+l4-1,   zord, zx,      zy,        zw);
-      lin2zord_internal(lin, start+l4,   start+2*l4-1, zord, zx,      zy+ls/2,   zw);
-      lin2zord_internal(lin, start+2*l4, start+3*l4-1, zord, zx,      zy+ls,     zw);
-      lin2zord_internal(lin, start+3*l4, end,          zord, zx,      zy+3*ls/2, zw);
-    } else {
-      lin2zord_internal(lin, start,      start+l4-1,   zord, zx,      zy,        zw);
-      lin2zord_internal(lin, start+l4,   start+2*l4-1, zord, zx+ls/2, zy,        zw);
-      lin2zord_internal(lin, start+2*l4, start+3*l4-1, zord, zx,      zy+ls/2,   zw);
-      lin2zord_internal(lin, start+3*l4, end,          zord, zx+ls/2, zy+ls/2,   zw);
+  int length = (end-start+1);
+  if (length <=4) { // Base case
+    switch (length) { // Fallthrough intentional
+      case 4:    *(zord+(zx+1)+(zy+1)*zw) = lin[start+3];
+      case 3:    *(zord+(zx+0)+(zy+1)*zw) = lin[start+2];
+      case 2:    *(zord+(zx+1)+(zy+0)*zw) = lin[start+1];
+      case 1:    *(zord+(zx+0)+(zy+0)*zw) = lin[start+0];
     }
+  } else if (ls > zw) {
+    lin2zord_internal(lin, start,      start+l4-1,   zord, zx,      zy,        zw);
+    lin2zord_internal(lin, start+l4,   start+2*l4-1, zord, zx,      zy+ls/2,   zw);
+    lin2zord_internal(lin, start+2*l4, start+3*l4-1, zord, zx,      zy+ls,     zw);
+    lin2zord_internal(lin, start+3*l4, end,          zord, zx,      zy+3*ls/2, zw);
+  } else {
+    lin2zord_internal(lin, start,      start+l4-1,   zord, zx,      zy,        zw);
+    lin2zord_internal(lin, start+l4,   start+2*l4-1, zord, zx+ls/2, zy,        zw);
+    lin2zord_internal(lin, start+2*l4, start+3*l4-1, zord, zx,      zy+ls/2,   zw);
+    lin2zord_internal(lin, start+3*l4, end,          zord, zx+ls/2, zy+ls/2,   zw);
   }
 }
 
 
 int lin2zord(unsigned char *lin, unsigned char *zord, int n, int zw)
 {
-  if (!isPowerOfFour(n)) return -1;
-  lin2zord_internal(lin, 0, n-1, zord, 0, 0, zw);
+  int i;
+  if (!IsPowerofTwo(zw)) return -1; // Don't even try to correct bad widths
+  if (zw*zw == n) { // Square
+    lin2zord_internal(lin, 0, n-1, zord, 0, 0, zw);
+  } else if (zw*zw < n) { // Tall
+    // Repeat down to reach n
+    for (i = 0; i*zw*zw < n; i++) {
+      lin2_zord_internal(lin, i*zw*zw, ,zord,
+    }
+  } else { // zw*zw > n  // Wide
+    // Height is the largest power of two less than n
+    // Repeat across to reach zw
+  }
   return 0;
 }
 
